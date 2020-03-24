192f09f3c065558fa91354da2774eff3418e0082
prateek93a <prateek93a@gmail.com>
2020-03-24 16:45:28 +0530

    changed-imports

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import * as core from '@actions/core';
-import * as github from '@actions/github';
+const core = require('@actions/core');
+const github = require('@actions/github');
 const Filter = require('bad-words'),
 filter = new Filter();
 
